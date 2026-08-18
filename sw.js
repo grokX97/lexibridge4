68b37233fb4f5f5687416d44548ea0ff8fc29b59
grokX97 <77411324+grokX97@users.noreply.github.com>
2026-08-18 20:30:00 +0800

    Refresh offline cache for vocabulary pagination fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='lexibridge4-v2';
+const C='lexibridge4-v3';
 const A=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./vocab/meta.js',...Array.from({length:10},(_,i)=>`./vocab/part-${String(i).padStart(2,'0')}.js`)];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
